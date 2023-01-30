0eb3c22ceb78cb06d3bed321466e6b91f9803757
nothamor <i@nothamor.cn>
2023-01-30 13:24:32 +0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Noth Amor on 2023/1/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
