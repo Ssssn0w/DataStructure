3a54372e2f344c530006509dbcbe9d7af86517c1
nothamor <i@nothamor.cn>
2023-02-05 15:36:55 +0800

    单链表课后题

--- DataStructure/LinkList.h
+++ DataStructure/LinkList.h
@@ -247,7 +247,7 @@ void DropMinimumLNodeInLinkList(LinkList &L) {
     free(minNode);
 }
 
-// ******(还没太整明白) 试编写算法将带头节点的单链表就地逆置，所谓“就地”是指辅助空间复杂度为O(1)
+// ******(还没太整明白) 试编写算法将带头节点的单链表就地逆置，所谓"就地"是指辅助空间复杂度为O(1)
 void ReverseLinkList(LinkList &L) {
     LNode *p = L->next, *r;
     L->next = NULL;
@@ -259,14 +259,94 @@ void ReverseLinkList(LinkList &L) {
     }
 }
 
+// 有一个带头节点的单链表L，设计一个算法使其元素递增有序
+void SortLinkList(LinkList &L) {
+    int linkListSize = 0;
+    LNode *t = L->next;
+    while (t != NULL) {
+        linkListSize++;
+        t = t->next;
+    }
+    
+    int temp = 0;
+    LNode *p = L->next, *pre = L;
+    for (int i = 0; i < linkListSize; i++) {
+        pre = L;
+        p = L->next;
+        for (int j = 0; j < linkListSize - i; j++) {
+            if (pre->data > p->data) {
+                temp = pre->data;
+                pre->data = p->data;
+                p->data = temp;
+            }
+            p = p->next;
+            pre = pre->next;
+        }
+    }
+}
+
+// 设在一个带表头节点的单链表中所有元素节点的数据值无序，试编写一个函数，删除表中所有介于给定的两个值之间的元素的元素（若存在）
+void DropValueBetweenSAndTInLinkList(LinkList &L, int s, int t) {
+    LNode *p = L->next, *pre = L;
+    while (p != NULL) {
+        if (p->data > s && p->data < t) {
+            pre->next = p->next;
+        }
+        
+        p = p->next;
+        pre = pre->next;
+    }
+}
+
+// 给定两个单链表，编写算法找出两个链表的公共节点
+void FindCommonNodeInTwoLinkList(LinkList L1, LinkList L2) {
+    LNode *p = L1->next, *t = L2->next;
+    while (p != NULL) {
+        t = L2->next;
+        while (t != NULL) {
+            if (p->data == t->data) {
+                std::cout << p->data << " ";
+            }
+            t = t->next;
+        }
+        p = p->next;
+    }
+    std::cout << std::endl;
+}
+
+// 给定一个带表头节点的单链表，设head为头指针，节点结构为(data,next), data为整型元素，next为指针，试写出算法：按递增次序输出单链表中各节点的数据元素，并释放节点所占的存储空间(要求：不允许使用数组作为辅助空间)
+void IncrementalPrintLinkListElement(LinkList L) {
+    while (L->next != NULL) {
+        LNode *p = L->next, *pre = L, *u;
+        while (p->next != NULL) {
+            if (p->next->data < pre->next->data) {
+                pre = p;
+            }
+            p = p->next;
+        }
+        
+        std::cout << pre->next->data << " ";
+        u = pre->next;
+        pre->next = u->next;
+        free(u);
+    }
+    std::cout << std::endl;
+    free(L);
+}
+
 void LinkListTest() {
     LinkList L;
+    LinkList L2;
     InitLinkList(L);
+    InitLinkList(L2);
     
-    LinkListInsert(L, 1);
-    LinkListInsert(L, 3);
-    LinkListInsert(L, 5);
     LinkListInsert(L, 7);
+    LinkListInsert(L, 5);
+    LinkListInsert(L, 3);
+    LinkListInsert(L, 1);
+    
+    LinkListInsert(L2, 1);
+    LinkListInsert(L2, 3);
     
     //int length = LinkListLength(L);
     //LinkListSearchByValue(L, 3);
@@ -276,7 +356,11 @@ void LinkListTest() {
     //DeleteAndFreeLNodeByValue(L, 3);
     //ReversePrintLinkList(L);
     //DropMinimumLNodeInLinkList(L);
-    ReverseLinkList(L);
+    //ReverseLinkList(L);
+    //SortLinkList(L);
+    //DropValueBetweenSAndTInLinkList(L, 1, 5);
+    //FindCommonNodeInTwoLinkList(L, L2);
+    IncrementalPrintLinkListElement(L);
 
     //std::cout << t->data << std::endl;
     //std::cout << length << std::endl;
